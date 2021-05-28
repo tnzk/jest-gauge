7f4a7e7b461a7597bb1d36f5ef8e79a2d3e70ed0
tnzk <tnzk.marge@gmail.com>
2021-05-28 17:30:08 +0900

    Have the default export has a function called

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import type { Config } from '@jest/types';
 import { buildTestPlanFromSpec } from './specs'
 import { loadSteps, buildTransformedSource } from './steps'
 
-function createTransformer(
-  sourceText: string,
-  sourcePath: string,
-  config: Config.ProjectConfig,
-  options?: TransformOptions
-): TransformedSource {
-  const specs = buildTestPlanFromSpec(sourceText);
-  const stepStore = loadSteps(sourcePath);
-  const transformed = buildTransformedSource(specs, stepStore);
-  return transformed;
+const jestGauge = {
+  process: function (
+    sourceText: string,
+    sourcePath: string,
+    config: Config.ProjectConfig,
+    options?: TransformOptions
+  ): TransformedSource {
+    const specs = buildTestPlanFromSpec(sourceText);
+    const stepStore = loadSteps(sourcePath);
+    const transformed = buildTransformedSource(specs, stepStore);
+    return transformed;
+  },
 }
 
-export default {
-  process: createTransformer
-}
+export const { process, } = jestGauge;
--- test/jest-gauge.test.ts
+++ test/jest-gauge.test.ts
@@ -1,6 +1,6 @@
 import { jest, test, expect, describe, beforeEach } from '@jest/globals'
-import transformer from '../src/index'
+import { process } from '../src/index'
 
 test('has at least a method called `process`', () => {
-    expect(transformer.process).toBeDefined();
+    expect(process).toBeDefined();
 })
