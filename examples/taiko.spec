d525d73d8d36583b91cc56d503ce076427413828
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2021-06-17 23:34:48 +0900

    Fix a scenareo sentence

--- examples/taiko.spec
+++ examples/taiko.spec
@@ -5,4 +5,4 @@ Ordinary paragraphs are treated as comments, just ignored.
 ## Scenario: Users open the site and see the welcome page
 
 * Open "https://duckduckgo.com/"
-* The user sees a cute cucumber-looking white bird
+* The user sees name of the cute cucumber-looking white bird
--- examples/taiko/steps.js
+++ examples/taiko/steps.js
@@ -1,9 +1,10 @@
 test("Open <url>", async (url) => {
-  await openBrowser({ observe: true, headless: false });
+  // TODO: Using this and focus other window while tests are running causes a leaked worker process. Weird.
+  await openBrowser({ observe: false, headless: false });
   await goto(url);
 }, 30000);
   
-test("The user sees a cute cucumber-looking white bird", async () => {
+test("The user sees name of the cute cucumber-looking white bird", async () => {
   await write('what is the name of the bird on DuckDuckGo');
   await press('Enter');
   const existence = await text('Kettle').exists();
